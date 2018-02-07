04eac6765f9eb9c13a634783c40f42ccf876c6a1
Achintya Kumar <achintyasi@gmail.com>
2018-02-06 19:07:33 -0500

    changed hhCore gyro model

--- drive/HHSensorDrive.java
+++ drive/HHSensorDrive.java
@@ -1,12 +1,12 @@
 package hhCore.drive;
 
-import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
 
 public abstract class HHSensorDrive extends HHDrive {
 	private double correction = 0.1;
 
-	public abstract AnalogGyro gyro();
+	public abstract ADXRS450_Gyro gyro();
 	public abstract Encoder leftEncoder();
 	public abstract Encoder rightEncoder();
 	public abstract double getLeftEncoder();
