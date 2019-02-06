546fc73d1679303fb1e50225242a25c9c7dd41e6
Christian Colglazier <christian@cacolglazier.com>
2019-02-06 17:04:43 -0500

    Allow setting gyro angle and getting robot velocity and acceleration

--- subsystems/drive/HHSensorDrive.java
+++ subsystems/drive/HHSensorDrive.java
@@ -2,6 +2,7 @@ package hhCore.subsystems.drive;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.GyroBase;
+import edu.wpi.first.wpilibj.Timer;
 
 public abstract class HHSensorDrive<T extends GyroBase> extends HHDrive {
 	private double correction = 0.1;
@@ -11,8 +12,14 @@ public abstract class HHSensorDrive<T extends GyroBase> extends HHDrive {
 	public abstract Encoder rightEncoder();
 	public abstract double getLeftEncoder();
 	public abstract double getRightEncoder();
-	public abstract double getGyro();
-
+	private double lastLeftEncoder = 0.0;
+	private double lastRightEncoder = 0.0;
+	private double lastLeftVelocity = 0.0;
+	private double lastRightVelocity = 0.0;
+	private double lastLeftAcceleration = 0.0;
+	private double lastRightAcceleration = 0.0;
+	private double lastTime = Timer.getFPGATimestamp();
+	private double gyroOffset = 0.0;
 	/**
 	 * resetLeftEncoder() resets left encoder to zero count.
 	 */
@@ -35,6 +42,22 @@ public abstract class HHSensorDrive<T extends GyroBase> extends HHDrive {
 	}
 
 	/**
+	 * setGyro() sets the gyro to a given value.
+	 * @param angle angle of robot in degrees
+	 */
+	public void setGyro(double angle) {
+		gyroOffset = getGyro() - angle;
+	}
+
+	/**
+	 * getGyro() returns angle of the robot.
+	 * @return angle of robot in degrees
+	 */
+	public double getGyro() {
+		return (gyro().getAngle() % 360) - gyroOffset;
+	}
+
+	/**
 	 * setCorrection() setter for correction value of driving.
 	 * @param correction speed to correct the robot
 	 */
@@ -54,5 +77,81 @@ public abstract class HHSensorDrive<T extends GyroBase> extends HHDrive {
 		} else {
 			pidDrive(0, speed);
 		}
+	}
+
+	/**
+	 * init() add to robot init to initialize velocity and acceleration tracking.
+	 */
+	public void init() {
+		lastTime = Timer.getFPGATimestamp();
+		resetLeftEncoder();
+		resetRightEncoder();
+		lastLeftEncoder = 0.0;
+		lastRightEncoder = 0.0;
+		lastLeftVelocity = 0.0;
+		lastRightVelocity = 0.0;
+		lastLeftAcceleration = 0.0;
+		lastRightAcceleration = 0.0;
+	}
+
+	/**
+	 * update() add to robot periodic to update velocity and acceleration.
+	 */
+	public void update() {
+		double currentTime = Timer.getFPGATimestamp();
+		double dt = currentTime - lastTime;
+
+		// Update velocity
+		double dl = getLeftEncoder() - lastLeftEncoder;
+		double dr = getRightEncoder()- lastRightEncoder;
+		double lVelocity = dl/dt;
+		double rVelocity = dr/dt;
+
+		// Update acceleration
+		double dla = lVelocity - lastLeftVelocity;
+		double dra = rVelocity - lastRightVelocity;
+		double lAcceleration = dla/dt;
+		double rAcceleration = dra/dt;
+
+		//Update with new values
+		lastTime = currentTime;
+		lastLeftEncoder = getLeftEncoder();
+		lastRightEncoder = getRightEncoder();
+		lastLeftVelocity = lVelocity;
+		lastRightEncoder = rVelocity;
+		lastLeftAcceleration = lAcceleration;
+		lastRightAcceleration = rAcceleration;
+	}
+
+	/**
+	 * getLeftVelocity() returns left side robot velocity.
+	 * @return velocity of left side of robot
+	 */
+	public double getLeftVelocity() {
+		return lastLeftVelocity;
+	}
+
+	/**
+	 * getRightVelocity() returns right side robot velocity.
+	 * @return velocity of right side of robot
+	 */
+	public double getRightVelocity() {
+		return lastRightVelocity;
+	}
+
+	/**
+	 * getLeftAcceleration() returns left side robot acceleration.
+	 * @return acceleration of left side of robot
+	 */
+	public double getLeftAcceleration() {
+		return lastLeftAcceleration;
+	}
+
+	/**
+	 * getRightAcceleration() returns right side robot acceleration.
+	 * @return acceleration of right side of robot
+	 */
+	public double getRightAcceleration() {
+		return lastRightAcceleration;
 	}
 }
