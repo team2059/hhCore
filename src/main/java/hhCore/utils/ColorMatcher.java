c75c077e1aae7b2cf2054527058de5c6a2d1ac63
jsnal <jasonlongball@gmail.com>
2020-01-10 11:58:01 -0500

    finish up the color matcher and add some javadoc

--- src/main/java/hhCore/utils/ColorMatcher.java
+++ src/main/java/hhCore/utils/ColorMatcher.java
@@ -7,23 +7,45 @@ import com.revrobotics.ColorMatch;
 
 import edu.wpi.first.wpilibj.util.Color;
 
+/**
+ * Uses HHColorSensor to match incoming color values with passed in
+ * colors. You are easily able to change what color the sensor trys
+ * to match with and at what confidence level it should return.
+ */
 public class ColorMatcher {
 
     private final HHColorSensor colorSensor = new HHColorSensor();
     private final ColorMatch colorMatcher = new ColorMatch();
 
-    private double confidenceThreshold = 0.80;
+    private double confidenceThreshold;
 
+    /**
+     * Adds the target colors to the color matcher.
+     * @param targetColors array of colors that you want to try to match against
+     */
     public ColorMatcher(Color[] targetColors) {
-
-        // Initialize target colors in the color matcher
         for (int i = 0; i < targetColors.length; i++) {
             colorMatcher.addColorMatch(targetColors[i]);
         }
+    }
+
+    /**
+     * Adds the target colors to the color matcher and sets the confidence threshold.
+     * @param targetColors array of colors that you want to try to match against
+     * @param confidenceThreshold confidence threshold
+     */
+    public ColorMatcher(Color[] targetColors, double confidenceThreshold) {
+        this(targetColors);
+        this.confidenceThreshold = confidenceThreshold;
 
         colorMatcher.setConfidenceThreshold(confidenceThreshold);
     }
 
+    /**
+     * Compares the raw color from the sensor with the passed in target colors to find the closest
+     * color that matches. You may need to turn on the builtin LED for better results
+     * @return raw matched color value
+     */
     public Color getColor() {
         final Color detectedColor = colorSensor.getColor();
         final ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
@@ -31,11 +53,11 @@ public class ColorMatcher {
         return match.color;
     }
 
+    /**
+     * Gets the current confidence threshold 
+     * @return the current confidence threshold
+     */
     public double getConfidenceThreshold() {
         return confidenceThreshold;
-    }
-
-    public void setConfidenceThreshold(double confidenceThreshold) {
-        this.confidenceThreshold = confidenceThreshold;
     }
 }
--- src/main/java/hhCore/utils/sensor/HHColorSensor.java
+++ src/main/java/hhCore/utils/sensor/HHColorSensor.java
@@ -4,8 +4,16 @@ import edu.wpi.first.wpilibj.I2C;
 
 import com.revrobotics.ColorSensorV3;
 
+/**
+ * Wrapper around the ColorSensorV3 that maps the port to the I2C port
+ * on the RoboRio. This class is mainly used in ColorMatcher and isn't
+ * meant to be invoked directly.
+ */
 public class HHColorSensor extends ColorSensorV3 {
 
+    /**
+     * Maps the ColorSesnorV3 to the I2C port.
+     */
     public HHColorSensor() {
         super(I2C.Port.kOnboard);
     }
