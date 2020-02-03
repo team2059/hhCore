02307b708f5a36453543b7eb032f8965ed1fd330
jsnal <jasonlongball@gmail.com>
2020-02-03 09:47:26 -0500

    make sure eveything builds and format the code

--- src/main/java/hhCore/utils/vision/LimeLight.java
+++ src/main/java/hhCore/utils/vision/LimeLight.java
@@ -5,33 +5,24 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimeLight {
 
-    private NetworkTable table;
+  private NetworkTable table;
 
-    public LimeLight() {
-        table = NetworkTableInstance.getDefault().getTable("limelight");
-    }
+  public LimeLight() {
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+  }
 
-    /**
-     * @return if limelight has found a target
-     */
-    public boolean isTargetFound() {
-        return table.getEntry("tv").getDouble(0.0) != 0.0;
-    }
+  /** @return if limelight has found a target */
+  public boolean isTargetFound() {
+    return table.getEntry("tv").getDouble(0.0) != 0.0;
+  }
 
-    /**
-     * @return crosshair offset from center on x axis
-     */
-    public double getHorizontalOffset() {
-        return table.getEntry("tx").getDouble(0.0);
-    }
+  /** @return crosshair offset from center on x axis */
+  public double getHorizontalOffset() {
+    return table.getEntry("tx").getDouble(0.0);
+  }
 
-    /**
-     * @return crosshair offset from center on y axis
-     */
-    public double getVerticalOffset() {
-        return table.getEntry("ty").getDouble(0.0);
-    }
-
-
-
+  /** @return crosshair offset from center on y axis */
+  public double getVerticalOffset() {
+    return table.getEntry("ty").getDouble(0.0);
+  }
 }
