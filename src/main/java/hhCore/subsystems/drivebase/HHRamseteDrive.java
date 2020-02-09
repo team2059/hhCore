87cea5823a49ce2a34e8fe264f71bc53a17d9283
aksharS <aksharjha@gmail.com>
2020-02-09 09:38:22 -0500

    navX vendordeps not working

---
+++ src/main/java/hhCore/subsystems/drivebase/HHRamseteDrive.java
@@ -0,0 +1,12 @@
+package hhCore.subsystems.drivebase;
+
+public abstract class HHRamseteDrive extends HHDrive {
+
+    public abstract AHRS gyro;
+
+
+    @Override
+    public void driveBase(double x, double y) {
+
+    }
+}
